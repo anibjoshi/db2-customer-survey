31fb29cbe80b138bcafbe6ce11be7c653478349f
Aniruddha Joshi <anibjoshi@gmail.com>
2025-10-04 02:19:06 -0500

    Moved image to public

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -16,7 +16,7 @@ export const LandingPage: React.FC = () => {
         textAlign: 'center'
       }}>
         <img 
-          src="/image.png" 
+          src="/public/image.png" 
           alt="IBM Db2 Survey" 
           style={{ 
             maxWidth: '500px',
