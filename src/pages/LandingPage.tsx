f6e4729adb9aa1e452242b7f2efa3c95ef75730c
Aniruddha Joshi <anibjoshi@gmail.com>
2025-10-04 02:12:14 -0500

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,31 +4,38 @@ import { Heading } from '@carbon/react';
 export const LandingPage: React.FC = () => {
   return (
     <div style={{ 
-      maxWidth: '700px', 
-      margin: '8rem auto',
-      padding: '0 2rem',
-      textAlign: 'center'
+      height: 'calc(100vh - 48px)',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: '2rem',
+      overflow: 'hidden'
     }}>
-      <img 
-        src="/image.png" 
-        alt="IBM Db2 Survey" 
-        style={{ 
-          maxWidth: '500px',
-          width: '100%',
-          height: 'auto',
-          marginBottom: '3rem'
-        }} 
-      />
-      <Heading style={{ marginBottom: '1.5rem', fontSize: '2.5rem' }}>
-        IBM Db2 Customer Survey
-      </Heading>
-      <p style={{ 
-        fontSize: '1.25rem', 
-        lineHeight: '1.6',
-        opacity: 0.9
+      <div style={{ 
+        maxWidth: '700px', 
+        textAlign: 'center'
       }}>
-        Help us understand which database problems impact you most. Your feedback will guide our product roadmap.
-      </p>
+        <img 
+          src="/image.png" 
+          alt="IBM Db2 Survey" 
+          style={{ 
+            maxWidth: '500px',
+            width: '100%',
+            height: 'auto',
+            marginBottom: '3rem'
+          }} 
+        />
+        <Heading style={{ marginBottom: '1.5rem', fontSize: '2.5rem' }}>
+          IBM Db2 Customer Survey
+        </Heading>
+        <p style={{ 
+          fontSize: '1.25rem', 
+          lineHeight: '1.6',
+          opacity: 0.9
+        }}>
+          Help us understand which database problems impact you most. Your feedback will guide our product roadmap.
+        </p>
+      </div>
     </div>
   );
 };
